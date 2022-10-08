34e77157d8923caf4cd5ff0996b0004dceba521b
Matheus Ferreira <jeronimomatheus05@gmail.com>
2022-10-08 02:08:20 -0300

    arquivo js adicionado

---
+++ app.js
@@ -0,0 +1,13 @@
+const span = document.querySelector('span')
+const btnStart = document.querySelector('#btnStart')
+const btnPause = document.querySelector('#btnPause')
+const btnClean = document.querySelector('#btnClean')
+
+let interval
+
+btnStart.onclick = () => {
+    btnStart.disabled = true
+    btnStart.disabled = false
+
+    let value
+}
